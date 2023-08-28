1aed8ecf9507d52857c183d6c16023f429530f72
romain.caprais@mail.polimi.it <romain.caprais@mail.polimi.it>
2023-08-28 17:59:18 +0200

    working binary adder up to 7

--- project_3/project_3.runs/synth_1/main.tcl
+++ project_3/project_3.runs/synth_1/main.tcl
@@ -71,7 +71,9 @@ proc create_report { reportName command } {
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
 set_param chipscope.maxJobs 2
-set_param synth.incrementalSynthesisCache C:/Users/pegas/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-17324-DESKTOP-FFCQFA0/incrSyn
+set_param synth.incrementalSynthesisCache C:/Users/pegas/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-18600-DESKTOP-FFCQFA0/incrSyn
+set_param xicom.use_bs_reader 1
+set_msg_config -id {Common 17-41} -limit 10000000
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
@@ -92,7 +94,6 @@ OPTRACE "Adding files" START { }
 read_vhdl -library xil_defaultlib {
   E:/ISEN/VIVADO_stage_m1/PicoBlazeGIT01/project_3/kcpsm6.vhd
   E:/ISEN/VIVADO_stage_m1/PicoBlazeGIT01/project_3/prog2.vhd
-  E:/ISEN/VIVADO_stage_m1/PicoBlazeGIT01/project_3/prog.vhd
   E:/ISEN/VIVADO_stage_m1/PicoBlazeGIT01/project_3/main.vhd
 }
 OPTRACE "Adding files" END { }
